8cfa34d167943486d88f3650841aee45d7d50c52
mohammad4Ak <mohammad13471356@gmail.com>
2024-11-15 23:51:45 +0330

    Update productData.ts
    remove '/' from first of image url

--- data/productData.ts
+++ data/productData.ts
@@ -17,7 +17,7 @@ export const products: Product[] = [
             'دارای کف طبی 👉\n' +
             'دور دوخت       👉\n' +
             'سبک و راحت',
-        image: '/images/products/1.jpg',
+        image: 'images/products/1.jpg',
         rating: 5,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -27,7 +27,7 @@ export const products: Product[] = [
     {
         id: 2,
         name: 'کفش سالومون مردانه و زنانه Salomon LAB مدل XT-6',
-        image: '/images/products/2.jpg',
+        image: 'images/products/2.jpg',
         rating: 4,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -37,7 +37,7 @@ export const products: Product[] = [
     {
         id: 3,
         name: 'کفش سالومون LAB مردانه و زنانه مدل XT-6',
-        image: '/images/products/3.jpg',
+        image: 'images/products/3.jpg',
         rating: 3,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -47,7 +47,7 @@ export const products: Product[] = [
     {
         id: 4,
         name: 'کفش سالومون مردانه و زنانه Salomon LAB مدل XT-6',
-        image: '/images/products/4.jpg',
+        image: 'images/products/4.jpg',
         rating: 2,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -64,7 +64,7 @@ export const products2: Product[] = [
             'کیفیت چینی👉\n' +
             'دارای کف طبی 👉\n' +
             'سبک و راحت',
-        image: '/images/products/5.webp',
+        image: 'images/products/5.webp',
         rating: 5,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -74,7 +74,7 @@ export const products2: Product[] = [
     {
         id: 2,
         name: 'کفش سالومون مردانه و زنانه Salomon LAB مدل XT-6',
-        image: '/images/products/6.webp',
+        image: 'images/products/6.webp',
         rating: 4,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -84,7 +84,7 @@ export const products2: Product[] = [
     {
         id: 3,
         name: 'کفش سالومون LAB مردانه و زنانه مدل XT-6',
-        image: '/images/products/7.webp',
+        image: 'images/products/7.webp',
         rating: 3,
         originalPrice: 3900000,
         discountedPrice: 2730000,
@@ -94,7 +94,7 @@ export const products2: Product[] = [
     {
         id: 4,
         name: 'کفش سالومون مردانه و زنانه Salomon LAB مدل XT-6',
-        image: '/images/products/8.webp',
+        image: 'images/products/8.webp',
         rating: 2,
         originalPrice: 3900000,
         discountedPrice: 2730000,
